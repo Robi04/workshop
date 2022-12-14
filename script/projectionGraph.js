c7d8fc1fbbe06a917cfa71f234757b28c785aa3a
Kiol-Ice <ramadiel2001@gmail.com>
2022-12-14 11:48:44 +0100

    Graph projection ok

--- script/projectionGraph.js
+++ script/projectionGraph.js
@@ -1,92 +1,152 @@
+var annees = [];
+for (let i = 18; i <= 70; i++) {
+  annees.push('20' + i.toString());
+}
+
+var ProjCentrale = [];
+var ProjBasse = [];
+var ProjHaute = [];
+
 var optionsProj = {
-  series: [{
-    name: "Session Duration",
-    data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+  series: [
+    {
+      name: 'Projection haute',
+      data: ProjHaute
+    },
+    {
+      name: "Projection centrale",
+      data: ProjCentrale
+    },
+    {
+      name: "Projection basse",
+      data: ProjBasse
+    },
+    
+  ],
+  chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    },
   },
-  {
-    name: "Page Views",
-    data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
-  },
-  {
-    name: 'Total Visits',
-    data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
-  }
-],
-chart: {
-  height: 350,
-  type: 'line',
-  zoom: {
+  dataLabels: {
     enabled: false
   },
-},
-dataLabels: {
-  enabled: false
-},
-stroke: {
-  width: [5, 7, 5],
-  curve: 'straight',
-  dashArray: [0, 8, 5]
-},
-title: {
-  text: 'Page Statistics',
-  align: 'left',
-  style: {
-    color: '#FFFFFF'
-}
-},
-legend: {
-  tooltipHoverFormatter: function(val, opts) {
-    return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
+  stroke: {
+    width: [3, 5, 3],
+    curve: 'smooth',
+    dashArray: [5, 0, 5]
   },
-  labels: {
-    colors: "#FFFFFF",
-    useSeriesColors: false
-}
-},
-markers: {
-  size: 0,
-  hover: {
-    sizeOffset: 6
-  }
-},
-xaxis: {
-  categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
-  '10 Jan', '11 Jan', '12 Jan'],
-  labels : {
+  title: {
+    text: 'Projection de la population ligérienne jusqu\'en 2070 selon 3 scénarios',
+    align: 'left',
     style: {
       color: '#FFFFFF'
-  }
-  }
-},
-tooltip: {
-  y: [
-    {
-      title: {
-        formatter: function (val) {
-          return val + " (mins)"
-        }
-      }
+    }
+  },
+  legend: {
+    tooltipHoverFormatter: function(val, opts) {
+      return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
     },
-    {
-      title: {
-        formatter: function (val) {
-          return val + " per session"
-        }
-      }
-    },
-    {
-      title: {
-        formatter: function (val) {
-          return val;
-        }
+    labels: {
+      colors: "#FFFFFF",
+      useSeriesColors: false
+    }
+  },
+  markers: {
+    size: 0,
+    hover: {
+      sizeOffset: 6
+    }
+  },
+  xaxis: {
+    categories: annees,
+    labels : {
+      style: {
+        colors: '#FFFFFF'
       }
     }
-  ]
-},
-grid: {
-  borderColor: '#f1f1f1',
-}
+  },
+  yaxis: {
+    title: {
+      text :'Population en milliers',
+      style : {
+        color : '#FFFFFF'
+      }
+    },
+    labels: {
+      style : {
+        colors: "#FFFFFF"
+      } 
+    }
+  },
+  tooltip: {
+    y: [
+      {
+        title: {
+          formatter: function (val) {
+            return val
+          }
+        }
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val
+          }
+        }
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val;
+          }
+        }
+      }
+    ]
+  },
+  grid: {
+    borderColor: '#f1f1f1',
+  }
 };
 
-var chartProj = new ApexCharts(document.querySelector("#projGraph"), optionsProj);
-chartProj.render();
+const urlProj = "./Data/Projection2070.json";
+
+const promiseLoadJsonProjection = new Promise((resolve, reject) =>{
+  $.getJSON(urlProj, function(data) {
+    
+    // Projection centrale
+    for(var key in data.Centrale) {
+      var obj = data.Centrale[key];
+      Object.values(obj).forEach(val => {
+        if (val != '42' && val != 'Loire'){ ProjCentrale.push(val); } 
+      });
+    }
+    
+    // Projection  basse
+    for(var key in data.Basse) {
+      var obj = data.Basse[key];
+      Object.values(obj).forEach(val => {
+        if (val != '42' && val != 'Loire'){ ProjBasse.push(val); } 
+      });
+    }
+    
+    // Projection haute
+    for(var key in data.Haute) {
+      var obj = data.Haute[key];
+      Object.values(obj).forEach(val => {
+        if (val != '42' && val != 'Loire'){ ProjHaute.push(val); } 
+      });
+    }
+    resolve('load ok');
+  });
+});
+
+promiseLoadJsonProjection.then((resolve) => {
+  console.log(resolve);
+  
+  var chartProj = new ApexCharts(document.querySelector("#projGraph"), optionsProj);
+  chartProj.render();
+});
+
